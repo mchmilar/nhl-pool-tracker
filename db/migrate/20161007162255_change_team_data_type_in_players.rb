062798de2189e1cc750c8104360c74d07daa1288
Mark Chmilar <markchmilar@gmail.com>
2016-10-13 20:43:41 +0000

    fixed migration for team id in players table to work with pg

--- db/migrate/20161007162255_change_team_data_type_in_players.rb
+++ db/migrate/20161007162255_change_team_data_type_in_players.rb
@@ -1,6 +1,7 @@
 class ChangeTeamDataTypeInPlayers < ActiveRecord::Migration
   def change
     rename_column :players, :team, :team_id
-    change_column :players, :team_id, :integer
+    #change_column :players, :team_id, :integer
+    execute 'ALTER TABLE players ALTER COLUMN team_id TYPE integer USING team_id::integer'
   end
 end
