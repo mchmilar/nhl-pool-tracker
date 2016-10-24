499cd37ee4b05dfbdf67ab228bc6e05e3b37fe7e
Mark Chmilar <mark.chmilar@ericsson.com>
2016-10-24 17:32:28 -0400

    remove admin migration

--- db/migrate/20161008003242_add_index_to_admins_email.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToAdminsEmail < ActiveRecord::Migration
-  def change
-    add_index :admins, :email, unique: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -16,15 +16,6 @@ ActiveRecord::Schema.define(version: 20161023140451) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "admins", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
-
   create_table "groups", force: :cascade do |t|
     t.string  "name"
     t.integer "op_id_number"
